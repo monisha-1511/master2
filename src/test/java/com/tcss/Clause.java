55d34bdde8b5458f4ab28ee1c5a2a2ede66c4631
monisha-1511 <monishav415@gmail.com>
2024-06-04 23:26:32 +0530

    saved

--- src/test/java/com/tcss/Clause.java
+++ src/test/java/com/tcss/Clause.java
@@ -8,24 +8,24 @@ public class Clause {
 		
 		System.out.println("method2");
 	}
-<<<<<<< HEAD
+
 	public void method4() {
 		System.out.println("method4");
 	}
-	public void method5() {
+	public void method7() {
 		System.out.println("method4");
 	}
 
-=======
+
 public void method3() {
 	System.out.println("mehod");
 }
 
-public void method4() {
-	System.out.println("mehod");
-}
 public void method5() {
 	System.out.println("mehod");
 }
->>>>>>> 76e9c308754511051471feb65dc4d15b9eb5693b
+public void method6() {
+	System.out.println("mehod");
+}
+
 }
